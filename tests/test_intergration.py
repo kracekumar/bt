7c9e7bffc2eca6a35cff2f1cfd8da2877df63798
Kracekumar R <me@kracekumar.com>
2016-10-17 02:11:34 -0400

    Use curio for tests

--- tests/test_intergration.py
+++ tests/test_intergration.py
@@ -1,28 +1,24 @@
 # -*- coding: utf-8 -*-
 
 import os
-import asyncio
+import curio
 
 from bt import Client
 
 
 def test_tom_torrent_download():
     client = Client()
-    loop = asyncio.get_event_loop()
-    task = loop.create_task(client.download(
+    curio.run(client.download(
         path='tom.torrent',
         savedir=b''))
-    loop.run_until_complete(task)
 
     assert os.path.getsize('flag.jpg') == 1277987
 
 
 def test_ubuntu_torrent_download():
     client = Client()
-    loop = asyncio.get_event_loop()
-    task = loop.create_task(client.download(
+    curio.run(client.download(
         path='ubuntu-16.04.1-desktop-amd64.iso.torrent',
         savedir=b''))
-    loop.run_until_complete(task)
 
     assert os.path.getsize('ubuntu-16.04.1-desktop-amd64.iso') == 1277987
